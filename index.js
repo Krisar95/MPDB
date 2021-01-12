494ee02cb3dd6d03a45ce02b97b7fef522332c3f
Krisar95 <58483513+Krisar95@users.noreply.github.com>
2021-01-12 22:11:47 +0100

    colfix
    dotenv added instead of potentially hazardous token file

--- index.js
+++ index.js
@@ -1,50 +1,29 @@
 const Discord = require('discord.js');
-const conf = require('./bconf.json');
+require('dotenv').config()
 const func = require('./functions');
-const fs = require("fs");
 const db = require('mongodb').MongoClient;
-const uri = conf.uri;
+const uri = process.env.URI;
+const token = process.env.TOKEN;
 const mongo = new db(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const bot = new Discord.Client();
 
-// Read directory /commands/
-/*fs.readdir("./commands", (err, files) => {
-  if(err) console.log(err);
-  let jsFile = files.filter(f => f.split(".").pop() === "js")
-  if(jsFile.length <= 0) {
-    console.log("Couldn't find any commands!")
-    return;
-  }
+func.fetchCommands();
 
-  // Loops through the /commands/ folder to find out what commands are there and usable
-  jsFile.forEach((f, i) => {
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded successfully.`);
-    bot.commands.set(props.name, props)
-    bot.other.set(props.alias, props)
+mongo.connect(async err => {
+
+  const db = mongo.db("mpdp");
+  const cursor = db.collection("mpdp_config")
+  const res = await cursor.findOne({})
+
+  var version = "1.1";
+  // lets you know the bot is online and ready to be used
+  bot.on('ready', () => {
+    console.log(`${bot.user.username} is online!`);
+    bot.user.setActivity(`Alpha 0.275.26 (${res.prefix})`, {
+        type: "WATCHING"
+      }
+    );
   });
-});*/
-
-  func.fetchCommands();
-
-  bot.commands = new Discord.Collection();
-  bot.other = new Discord.Collection();
-
-  mongo.connect(async err => {
-
-    const db = mongo.db("mpdp");
-    const cursor = db.collection("mpdp_config")
-    const res = await cursor.findOne({})
-
-    var version = "1.1";
-    // lets you know the bot is online and ready to be used
-      bot.on('ready', () => {
-        console.log(`${bot.user.username} is online!`);
-        bot.user.setActivity(`Alpha 0.275.26 (${res.prefix})`, {
-            type: "WATCHING"
-          }
-        );
-      });
 
 
     bot.on('messageUpdate', async(oldMessage, newMessage) => {
@@ -74,16 +53,13 @@ const bot = new Discord.Client();
     bot.on('message', async message => {
       if(message.author.bot) return;
       if(!message.guild) return;
-      if(!message.content.startsWith(prefix)) return;
+      if(!message.content.startsWith(res.prefix)) return;
       if(message.guild.id !== "653024881348182016") return message.channel.send("I'm currently being rewritten, my commands are useless here.")
-      
       // Command handler
       func.runCommands(message);
       // End command handler
     });
 
-
-
 });
 
-bot.login(conf.token);
+bot.login(token);
